dd94c398735bc55e69f7a580ae60c9661844282e
Damon-mac <weidongzhu36@126.com>
2018-12-28 20:45:45 +0800

    加入redux

--- server/server.js
+++ server/server.js
@@ -6,15 +6,43 @@ mongoose.connect(DB_URL)
 mongoose.connection.on('connected', function() {
   console.log('mongo connect success')
 })
+// 类似于mysql的表 mongo里有文档, 字段的概念
+const User = mongoose.model('user', new mongoose.Schema({
+  user: {type: String, require: true},
+  age: {type: Number, require: true}
+}))
+// 新增数据
+// User.create({
+//   user: 'yinghua',
+//   age: 28
+// }, (err, doc) => {
+//   if (!err) {
+//     console.log(doc)
+//   } else {
+//     console.log(err)
+//   }
+// })
 
 const app = express()
+// 删除age为30的所有数据
+// User.remove({age: 30}, (err, doc) => {
+//   console.log(doc)
+// })
+// 更新数据
+// User.update({'user': 'yinghua'}, {'$set': {age: 27}}, (err, doc) => {
+//   console.log(doc)
+// })
 
 app.get('/', function(req, res) {
   res.send('hello world')
 })
 
 app.get('/data', function(req, res) {
-  res.json({name: 'mac', type: 'IT'})
+  // 查找数据, 第一个参数是过滤的对象
+  User.find({user: 'yinghua'}, (err, doc) => {
+    res.json(doc)
+  })
+  // res.json({name: 'mac', type: 'IT'})
 })
 
 app.listen(3000, function(req, res) {
